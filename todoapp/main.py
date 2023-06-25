d5c3268ea4770c7c02b76493661ce438963ee97a
Pratham <pratham.tikkisetty@gmail.com>
2023-06-25 12:32:50 +0530

    addon

---
+++ todoapp/main.py
@@ -0,0 +1,16 @@
+
+
+from fastapi import FastAPI,Depends,Path
+
+import models
+from models import Todos
+from database import engine
+
+from routers import auth,todos
+
+app=FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router)
+app.include_router(todos.router)
---
+++ todoapp/models.py
@@ -0,0 +1,25 @@
+from database import Base 
+from sqlalchemy import Column,Integer,String,Boolean,ForeignKey
+
+
+class Users(Base):
+    __tablename__='users'
+
+    id=Column(Integer,primary_key=True,index=True)
+    email=Column(String,primary_key=True)
+    username=Column(String,unique=True)
+    first_name=Column(String)
+    last_name=Column(String)
+    hashed_password=Column(Boolean,default=True)
+    is_active=Column(String)
+
+    role=Column(String)
+
+class Todos(Base):
+    __tablename__ = 'todos'
+    id = Column(Integer,primary_key=True,index=True)
+    title=Column(String)
+    description=Column(String)
+    priority=Column(Integer)
+    complete=Column(Boolean,default=False)
+    owner_id=Column(Integer,ForeignKey("user.id"))
